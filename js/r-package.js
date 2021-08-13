94d8e6d0bb93666b58bc417fb413fd92e0a6c847
Md Naimur Rahman Hira <76173840+MdNaimurRahmanHira@users.noreply.github.com>
2021-08-14 02:17:25 +0600

    Update r-package.js

--- js/r-package.js
+++ js/r-package.js
@@ -1,16 +1,18 @@
 $(document).ready(function () {
-    $(".menu-hide-btn").click(function () { 
+    $(".menu-hide-btn").click(function () {
+        $(".menu-hide-btn img").toggle()
+        $(".menu-hide-btn i").toggle()
+
         $(".left-menu").toggleClass("left-menu-none")
         $(".right-content").toggleClass("col").removeClass("col-12")
-      
-     })
-     
-        if (window.matchMedia('(max-width: 767px)').matches) {
-            $(".menu-hide-btn").click(function (){
-                $(".left-menu").toggleClass("menu-for-small-screen")
-            })
 
-           
-        } 
+    })
+
+    if (window.matchMedia('(max-width: 767px)').matches) {
+        $(".menu-hide-btn").click(function () {
+            $(".left-menu").toggleClass("menu-for-small-screen")
+        })
+
+    }
 
 })
